9f7cbc1471d2078a781585d4099985849e6452bb
Hasan <qasanliqasn@gmail.com>
2025-07-15 15:35:25 +0400

    first

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -14,12 +14,13 @@ function Login() {
     setError('');
 
     try {
-      const response = await fetch('http://localhost:5000/api/auth/login', {
+      const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/auth/login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
-        credentials: 'include' // 👈 ADD THIS
+        credentials: 'include'
       });
+
 
       if (!response.ok) {
         const err = await response.json();
