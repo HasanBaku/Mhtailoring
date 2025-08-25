0ad951225db690c7d73ebc48d90d7cb5cb3cc01c
Hasan Hasanli <61019174+HasanBaku@users.noreply.github.com>
2025-08-25 18:53:06 +0400

    Update authenticateJWT.js

--- backend/middleware/authenticateJWT.js
+++ backend/middleware/authenticateJWT.js
@@ -8,7 +8,7 @@ const authenticateToken = async (req, res, next) => {
     if (!token) return res.status(401).json({ error: 'No token provided' });
 
   try {
-    const user = jwt.verify(token, process.env.JWT);
+    const user = jwt.verify(token, process.env.JWT_SECRET);
 
     // Ensure required fields
     if (!user.userId || !user.role) {
