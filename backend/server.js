7565c4752d1a366d7a7c68277c9c8260b09e935c
Hasan Hasanli <61019174+HasanBaku@users.noreply.github.com>
2025-08-25 10:02:52 +0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://mhtailoring.onrender.com'
+  'https://mhtailoring-front.onrender.com' 
 ];
 
 const corsOptions = {
