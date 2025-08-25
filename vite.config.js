bce85bdc6293adbf7bac5082629214967baa5d72
Hasan Hasanli <61019174+HasanBaku@users.noreply.github.com>
2025-08-25 18:21:04 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/', // ✅ THIS is critical for SPA routing
+  base: '/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: 'index.html'
+    }
+  }
 });
