2a56fa88ed4573dd851731d6555d96b558c4260e
Hasan Hasanli <61019174+HasanBaku@users.noreply.github.com>
2025-08-25 18:10:50 +0400

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,37 +1,18 @@
 import axios from 'axios';
 
-const instance = axios.create({
+const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL + '/api',
-  headers: { 'Content-Type': 'application/json' }
+  headers: {
+    'Content-Type': 'application/json'
+  }
 });
 
-// 🔥 Automatically add token to each request
-instance.interceptors.request.use(
-  config => {
-    const token = localStorage.getItem('authToken');
-    if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
-    }
-    return config;
-  },
-  error => Promise.reject(error)
-);
-
-
-instance.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response && error.response.status === 401) {
-      const err = error.response.data?.error;
-
-      if (err === 'TokenExpired' || err === 'No token provided' || err === 'User not found') {
-        localStorage.removeItem('authToken');
-        window.location.href = '/login';
-      }
-    }
-
-    return Promise.reject(error);
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('authToken');
+  if (token) {
+    config.headers['Authorization'] = `Bearer ${token}`;
   }
-);
+  return config;
+}, error => Promise.reject(error));
 
-export default instance;
+export default api;
